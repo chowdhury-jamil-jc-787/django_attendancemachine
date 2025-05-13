7dacb08185546656b5bb5a33d6d39e81ec0ae545
Jamil Chowdhury <jamilchowdhury@Jamils-Mac-mini.local>
2025-05-13 13:15:37 +0600

    daily-punches function time condition changed

--- attendancemachine/profiles/serializers.py
+++ attendancemachine/profiles/serializers.py
@@ -7,20 +7,28 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ['profile_img', 'birthday', 'phone_number']
+        extra_kwargs = {
+            'profile_img': {'required': False, 'allow_null': True},
+            'birthday': {'required': False, 'allow_null': True},
+            'phone_number': {'required': False, 'allow_null': True},
+        }
 
     def validate_profile_img(self, image):
-        # Extract current user from serializer context
-        user = self.context['request'].user
+        if not image:
+            return None  # support clearing the image with null
+
+        user = self.context.get('request').user
         username = slugify(user.username)
         now = datetime.now().strftime("%Y-%m-%d_%H%M%S")
 
-        # Rename uploaded image
+        # Rename the uploaded file
         ext = image.name.split('.')[-1]
         image.name = f"{now}_{username}.{ext}"
 
-        # Optional: Validate file type and size
+        # Validate file type and size
         max_size = 2 * 1024 * 1024  # 2 MB
         valid_extensions = ['jpg', 'jpeg', 'png']
+
         if image.size > max_size:
             raise serializers.ValidationError("Image size must be under 2MB.")
         if ext.lower() not in valid_extensions:
--- attendancemachine/profiles/urls.py
+++ attendancemachine/profiles/urls.py
@@ -1,8 +1,9 @@
 # profiles/urls.py
 
 from django.urls import path
-from .views import UpdateProfileView
+from .views import UpdateProfileView, RetrieveProfileView
 
 urlpatterns = [
     path('update/', UpdateProfileView.as_view(), name='update_profile'),
+    path('me/', RetrieveProfileView.as_view(), name='my_profile'),
 ]
